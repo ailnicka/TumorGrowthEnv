dff02fef17343e7b97a159f2ce1cd6226e522d08
Agnieszka Ilnicka <ajilnicka@gmail.com>
2020-12-01 18:54:27 +0100

    small fix

--- tumor_growth/__init__.py
+++ tumor_growth/__init__.py
@@ -2,5 +2,5 @@ from gym.envs.registration import register
 
 register(
     id='tumor_growth-v0',
-    entry_point='tumor_growth.envs:tumor_growth_env',
+    entry_point='tumor_growth.envs:TumorGrowthEnv',
 )
